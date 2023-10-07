836a61ac547dc423c6d321f2dc009bfdb941d908
Mike Bell <mdb036@gmail.com>
2023-10-07 13:10:47 +0100

    Test mode switching

--- bubbles/main.cpp
+++ bubbles/main.cpp
@@ -15,13 +15,52 @@ using namespace pimoroni;
 #define FRAME_WIDTH 1000
 #define FRAME_HEIGHT 600
 
-#define USE_PALETTE 0
+DVDisplay display;
 
 void on_uart_rx() {
     while (uart_is_readable(uart1)) {
         uint8_t ch = uart_getc(uart1);
         putc(ch, stdout);
     }
+}
+
+static constexpr int NUM_CIRCLES = 50;
+static struct Circle {
+  uint16_t x, y, size, grow;
+  uint32_t pen;
+} circles[NUM_CIRCLES];
+
+void setup_pen(PicoGraphicsDV* graphics, DVDisplay::Mode mode) {
+  graphics->create_pen(0, 0, 0);
+  graphics->create_pen(0xFF, 0xFF, 0xFF);
+
+  if (mode == DVDisplay::MODE_PALETTE) {
+    for (int i = 0; i < 25; ++i) {
+      graphics->create_pen_hsv(i * 0.04f, 1.0f, 1.0f);
+    }
+    for (int i = 0; i < 5; ++i) {
+      graphics->create_pen((i+3) * (255/8), 255, 255);
+    }
+  }
+
+  for(int i =0 ; i < 50 ; i++)
+  {
+    circles[i].size = (rand() % 50) + 1;
+    circles[i].grow = std::max(0, (rand() % 50) - 25);
+    circles[i].x = rand() % graphics->bounds.w;
+    circles[i].y = rand() % graphics->bounds.h;
+    if (mode == DVDisplay::MODE_PALETTE) {
+      circles[i].pen = 2 + (i >> 1);
+    } else {
+      circles[i].pen = graphics->create_pen_hsv(i * 0.02f, 1.0f, 1.0f);
+    }
+  }
+
+  display.set_scroll_idx_for_lines(1, 100, 200);
+  display.set_scroll_idx_for_lines(2, 300, FRAME_HEIGHT);
+  display.flip();
+  display.set_scroll_idx_for_lines(1, 100, 200);
+  display.set_scroll_idx_for_lines(2, 300, FRAME_HEIGHT);
 }
 
 int main() {
@@ -48,68 +87,18 @@ int main() {
 
   //sleep_ms(5000);
 
-  DVDisplay display;
-  display.init(DISPLAY_WIDTH, DISPLAY_HEIGHT, DVDisplay::MODE_RGB888, FRAME_WIDTH, FRAME_HEIGHT);
+  DVDisplay::Mode mode = DVDisplay::MODE_RGB888;
+  display.init(DISPLAY_WIDTH, DISPLAY_HEIGHT, mode, FRAME_WIDTH, FRAME_HEIGHT);
 
-#if USE_PALETTE
-  display.set_mode(DVDisplay::MODE_PALETTE);
-  PicoGraphics_PenDV_P5 graphics(FRAME_WIDTH, FRAME_HEIGHT, display);
-#else
-  PicoGraphics_PenDV_RGB888 graphics(FRAME_WIDTH, FRAME_HEIGHT, display);
-#endif
-
-  graphics.create_pen(0, 0, 0);
-  graphics.create_pen(0xFF, 0xFF, 0xFF);
-
-#if USE_PALETTE
-  for (int i = 0; i < 25; ++i) {
-    graphics.create_pen_hsv(i * 0.04f, 1.0f, 1.0f);
-  }
-  for (int i = 0; i < 5; ++i) {
-    graphics.create_pen((i+3) * (255/8), 255, 255);
-  }
-#endif
-
-  graphics.set_pen(0xFF, 0, 0);
-  printf(".\n");
-  graphics.clear();
-  printf("..\n");
-  display.set_scroll_idx_for_lines(0, 100, 200);
-  display.set_scroll_idx_for_lines(2, 300, FRAME_HEIGHT);
-  display.flip();
-  printf("...\n");
-  sleep_ms(2000);
-  graphics.set_pen(0, 0, 0xFF);
-  graphics.clear();
-  display.set_scroll_idx_for_lines(0, 100, 200);
-  display.set_scroll_idx_for_lines(2, 300, FRAME_HEIGHT);
-  display.flip();
+  PicoGraphicsDV* graphics = new PicoGraphics_PenDV_RGB888(FRAME_WIDTH, FRAME_HEIGHT, display);
+  setup_pen(graphics, mode);
 
   printf("Starting\n");
-  graphics.set_font("bitmap8");
+  graphics->set_font("bitmap8");
 
   Point scroll1 = {0, 0};
   Point scroll2 = {0, 0};
   int scroll_dir[2] = {1,1};
-
-  constexpr int NUM_CIRCLES = 50;
-  struct Circle {
-    uint16_t x, y, size, grow;
-    uint32_t pen;
-  } circles[NUM_CIRCLES];
-
-  for(int i =0 ; i < 50 ; i++)
-  {
-    circles[i].size = (rand() % 50) + 1;
-    circles[i].grow = std::max(0, (rand() % 50) - 25);
-    circles[i].x = rand() % graphics.bounds.w;
-    circles[i].y = rand() % graphics.bounds.h;
-#if USE_PALETTE
-    circles[i].pen = 2 + (i >> 1);
-#else
-    circles[i].pen = graphics.create_pen_hsv(i * 0.02f, 1.0f, 1.0f);
-#endif
-  }
 
   int frames = 0;
   while (true) {
@@ -120,40 +109,40 @@ int main() {
 
 #if 1
     for (int j = 0; j < FRAME_HEIGHT; ++j) {
-      graphics.set_pen(j & 0xFF, 0xFF, 0xFF);
-      graphics.pixel_span({0,j}, FRAME_WIDTH);
+      graphics->set_pen(j & 0xFF, 0xFF, 0xFF);
+      graphics->pixel_span({0,j}, FRAME_WIDTH);
     }
 #else
-    graphics.set_pen(0xFF, 0xFF, 0xFF);
-    graphics.clear();
+    graphics->set_pen(0xFF, 0xFF, 0xFF);
+    graphics->clear();
 #endif
 
 #if 0
     for (uint i = 0; i < 128; i++) {
       for (uint j = 0; j < 256; j++) {
         RGB555 col = (j << 7) | i;
-        graphics.set_pen((col << 3) & 0xF8, (col >> 2) & 0xF8, (col >> 7) & 0xF8);
-        graphics.pixel(Point(j, i));
+        graphics->set_pen((col << 3) & 0xF8, (col >> 2) & 0xF8, (col >> 7) & 0xF8);
+        graphics->pixel(Point(j, i));
       }
     }
 
     for (uint i = 0; i < 128; i++) {
       for (uint j = 0; j < 256; j++) {
-        graphics.set_pen((j << 7) | i);
-        graphics.pixel(Point(i, j+128));
+        graphics->set_pen((j << 7) | i);
+        graphics->pixel(Point(i, j+128));
       }
     }
 #endif
 
     for(int i =0 ; i < NUM_CIRCLES ; i++)
     {
-      graphics.set_pen(0, 0, 0);
-      graphics.circle(Point(circles[i].x, circles[i].y), circles[i].size);
+      graphics->set_pen(0, 0, 0);
+      graphics->circle(Point(circles[i].x, circles[i].y), circles[i].size);
 
       //RGB col = RGB::from_hsv(i * 0.02f, 1.0f, 1.0f);
-      //graphics.set_pen(col.r, col.g, col.b);
-      graphics.set_pen(circles[i].pen);
-      graphics.circle(Point(circles[i].x, circles[i].y), circles[i].size-2);
+      //graphics->set_pen(col.r, col.g, col.b);
+      graphics->set_pen(circles[i].pen);
+      graphics->circle(Point(circles[i].x, circles[i].y), circles[i].size-2);
       if (circles[i].grow) {
         circles[i].size++;
         circles[i].grow--;
@@ -162,18 +151,18 @@ int main() {
         if (circles[i].size == 0) {
           circles[i].size = 1;
           circles[i].grow = rand() % 75;
-          circles[i].x = rand() % graphics.bounds.w;
-          circles[i].y = rand() % graphics.bounds.h;
+          circles[i].x = rand() % graphics->bounds.w;
+          circles[i].y = rand() % graphics->bounds.h;
         }
       }
     }
 
 #if 0
-    uint x = 260; //rand() % graphics.bounds.w;
-    uint y = 468; //rand() % graphics.bounds.h;
+    uint x = 260; //rand() % graphics->bounds.w;
+    uint y = 468; //rand() % graphics->bounds.h;
     printf("Circle at (%d, %d)\n", x, y);
-    graphics.set_pen(0);
-    graphics.circle(Point(x, y), 25);
+    graphics->set_pen(0);
+    graphics->circle(Point(x, y), 25);
   #endif
 
     uint32_t render_time = time_us_32() - render_start_time;
@@ -183,8 +172,8 @@ int main() {
             gpio_get(BUTTON_Y) == 0 ? "Y" : " ", 
             display.is_button_x_pressed() ? "X" : " ",
             display.is_button_a_pressed() ? "A" : " ");
-    graphics.set_pen(0, 0, 0);
-    graphics.text(buffer, {500,10}, FRAME_WIDTH - 500, 3);
+    graphics->set_pen(0, 0, 0);
+    graphics->text(buffer, {500,10}, FRAME_WIDTH - 500, 3);
 
     uint32_t flip_start_time = time_us_32();
     display.flip();
@@ -194,8 +183,8 @@ int main() {
     //printf("%02x %02x\n", display.get_gpio(), display.get_gpio_hi());
 
 #if 0
-    display.set_display_offset(scroll1, 1);
-    display.set_display_offset(scroll2, 2);
+    display.setup_scroll_group(scroll1, 1);
+    display.setup_scroll_group(scroll2, 2);
     scroll1.x += scroll_dir[0];
     if (scroll1.x + DISPLAY_WIDTH > FRAME_WIDTH || scroll1.x < 0) {
       scroll_dir[0] = -scroll_dir[0];
@@ -213,5 +202,20 @@ int main() {
     display.set_gpio_hi_pull_down_all(~(frames & 0x3F));
     if (gpio_get(BUTTON_Y) == 0) display.set_led_level((uint8_t)frames);
     else display.set_led_heartbeat();
+
+    if (display.is_button_a_pressed()) {
+      delete graphics;
+      if (mode == DVDisplay::MODE_RGB888) {
+        graphics = new PicoGraphics_PenDV_P5(FRAME_WIDTH, FRAME_HEIGHT, display);
+        mode = DVDisplay::MODE_PALETTE;
+        display.set_mode(mode);
+      }
+      else {
+        graphics = new PicoGraphics_PenDV_RGB888(FRAME_WIDTH, FRAME_HEIGHT, display);
+        mode = DVDisplay::MODE_RGB888;
+        display.set_mode(mode);
+      }
+      setup_pen(graphics, mode);
+    }
   }
 }
